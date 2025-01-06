6edc543da7dd1c54391471d18e47bc8d1b83370f
lukas-haindl <lukas.haindl03@icloud.com>
2025-01-06 18:05:32 +0100

    implement gameboard and functions

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -37,22 +37,110 @@ public class Aufgabe1 {
     }
 
     private static int[] minimax(char[][] gameBoard, boolean player, int depth) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+
+        int[] retArray = new int[3]; // [row, col, score]
+
+        if (player) {
+            retArray[2] = Integer.MIN_VALUE;
+        } else {
+            retArray[2] = Integer.MAX_VALUE;
+        }
+
+        for (int row = 0; row < gameBoard.length; row++) {
+            for (int col = 0; col < gameBoard[row].length; col++) {
+                if (gameBoard[row][col] == ' ') { // Empty field
+                    gameBoard[row][col] = player ? 'X' : 'O';
+
+                    if (checkIfWinner(gameBoard, true)) {
+                        retArray[0] = row;
+                        retArray[1] = col;
+                        retArray[2] = -1;
+                    } else if (checkIfWinner(gameBoard, false)) {
+                        retArray[0] = row;
+                        retArray[1] = col;
+                        retArray[2] = 1;
+                    } else if (checkIfFull(gameBoard) || depth - 1 == 0) {
+                        retArray[0] = row;
+                        retArray[1] = col;
+                        retArray[2] = 0;
+                    } else {
+                        int[] tempArray = minimax(gameBoard, !player, depth - 1);
+
+                        if (player && tempArray[2] > retArray[2]) {
+                            retArray[0] = row;
+                            retArray[1] = col;
+                            retArray[2] = tempArray[2];
+                        } else if (!player && tempArray[2] < retArray[2]) {
+                            retArray[0] = row;
+                            retArray[1] = col;
+                            retArray[2] = tempArray[2];
+                        }
+                    }
+
+                    gameBoard[row][col] = ' '; // Undo move
+                }
+            }
+        }
+
+        return retArray;
     }
 
     private static boolean checkIfFull(char[][] gameBoard) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
+        for (char[] array : gameBoard) {
+            for (char element : array) {
+                if (element == ' ') {
+                    return true;
+                }
+            }
+        }
         return false; //Zeile kann geändert oder entfernt werden.
     }
 
     private static boolean checkIfWinner(char[][] gameBoard, boolean player) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return false; //Zeile kann geändert oder entfernt werden.
+        char winningSymbol = player ? 'X' : 'O';
+        //row Check
+        for (char[] array : gameBoard) {
+            if (array[0] == winningSymbol && array[1] == winningSymbol && array[2] == winningSymbol) {
+                return true;
+            }
+        }
+        //col Check
+        for (int col = 0; col < gameBoard[0].length; col++) {
+            if (gameBoard[0][col] == winningSymbol && gameBoard[1][col] == winningSymbol && gameBoard[2][col] == winningSymbol) {
+                return true;
+            }
+        }
+        //dia Check
+        return (gameBoard[0][0] == winningSymbol && gameBoard[1][1] == winningSymbol && gameBoard[2][2] == winningSymbol) ||
+                (gameBoard[0][2] == winningSymbol && gameBoard[1][1] == winningSymbol && gameBoard[2][0] == winningSymbol);//Zeile kann geändert oder entfernt werden.
     }
 
     private static void drawGameBoard(CodeDraw myDrawObj, char[][] gameBoard) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
+        //draw Gameboard-Lines
+        double thirdX = myDrawObj.getWidth() / 3.0;
+        double thirdY = myDrawObj.getHeight() / 3.0;
+        myDrawObj.drawLine(thirdX, 0, thirdX, myDrawObj.getHeight());
+        myDrawObj.drawLine(thirdX * 2, 0, thirdX * 2, myDrawObj.getHeight());
+
+        myDrawObj.drawLine(0, thirdY, myDrawObj.getWidth(), thirdY);
+        myDrawObj.drawLine(0, thirdY * 2, myDrawObj.getWidth(), thirdY * 2);
+        //draw Symbols
+        double posX = 0;
+        double posY = 0;
+        for (char[] array : gameBoard) {
+            for (char element : array) {
+                if (element == 'X') {
+                    myDrawObj.drawLine(posX, posY, posX + thirdX, posY + thirdY);
+                    myDrawObj.drawLine(posX, posY + thirdY, posX + thirdX, posY);
+                } else if (element == 'O') {
+                    myDrawObj.drawCircle(posX + thirdX / 2.0, posY + thirdY / 2.0, thirdX / 2.0);
+                }
+                posX += thirdX;
+            }
+            posY += thirdY;
+            posX = 0;
+        }
+        myDrawObj.show();
     }
 
 }
