e21f1414f9333707a83df134c873b27fb26d11a4
lukas-haindl <lukas.haindl03@icloud.com>
2025-01-07 14:03:43 +0100

    finish program

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -20,36 +20,118 @@ public class Aufgabe1 {
 
         boolean twoPlayer = false; //true ... human vs. human, false ... human vs. computer
         boolean player = true; //(1Player) human = true, computer = false, (2Player) human1 = true, human2 = false
-        int maxDepth = 4;
+        int maxDepth = 3;
         boolean gameRunning = true;
 
         drawGameBoard(myDrawObj, gameBoard);
 
         while (!myDrawObj.isClosed() && gameRunning) {
             if (myEventSC.hasMouseClickEvent()) {
-                // TODO: Implementieren Sie hier Ihre Lösung für den if-Zweig
+                MouseClickEvent currentClick = myEventSC.nextMouseClickEvent();
+                int mouseX = currentClick.getX();
+                int mouseY = currentClick.getY();
+                int third = size / 3;
+                int row = Integer.MIN_VALUE;
+                int col = Integer.MIN_VALUE;
+                char symbol = 'X';
+                if (twoPlayer && !player){
+                    symbol = 'O';
+                }
+                if (mouseX > 0 && mouseX < third && mouseY > 0 && mouseY < third) {
+                    System.out.println("row: 1, col: 1");
+                    row = 0;
+                    col = 0;
+                } else if (mouseX > third && mouseX < third * 2 && mouseY > 0 && mouseY < third){
+                    System.out.println("row: 1, col: 2");
+                    row = 0;
+                    col = 1;
+                } else if (mouseX > third * 2 && mouseX < size && mouseY > 0 && mouseY < third){
+                    System.out.println("row: 1, col: 3");
+                    row = 0;
+                    col = 2;
+                } else if (mouseX > 0 && mouseX < third && mouseY > third && mouseY < third * 2) {
+                    System.out.println("row: 2, col: 1");
+                    row = 1;
+                    col = 0;
+                } else if (mouseX > third && mouseX < third * 2 && mouseY > third && mouseY < third * 2){
+                    System.out.println("row: 2, col: 2");
+                    row = 1;
+                    col = 1;
+                } else if (mouseX > third * 2 && mouseX < size && mouseY > third && mouseY < third * 2){
+                    System.out.println("row: 2, col: 3");
+                    row = 1;
+                    col = 2;
+                } else if (mouseX > 0 && mouseX < third && mouseY > third * 2 && mouseY < size) {
+                    System.out.println("row: 3, col: 1");
+                    row = 2;
+                    col = 0;
+                } else if (mouseX > third && mouseX < third * 2 && mouseY > third * 2 && mouseY < size){
+                    System.out.println("row: 3, col: 2");
+                    row = 2;
+                    col = 1;
+                } else if (mouseX > third * 2 && mouseX < size && mouseY > third * 2 && mouseY < size){
+                    System.out.println("row: 3, col: 3");
+                    row = 2;
+                    col = 2;
+                }
+                if (row != Integer.MIN_VALUE && col != Integer.MIN_VALUE && gameBoard[row][col] == ' ') {
+                    gameBoard[row][col] = symbol;
+                    if (checkIfWinner(gameBoard, player)){
+                        gameRunning = false;
+                        if (player){
+                            System.out.println("Player 1 wins!");
+                        } else {
+                            System.out.println("Player 2 wins!");
+                        }
+                    };
+                    player = !player;
+                } else{
+                    System.out.println("Sie müssen in ein leeres Feld klicken!");
+                }
+
             } else if (!twoPlayer && !player) {
-                // TODO: Implementieren Sie hier Ihre Lösung für den else-if-Zweig
+                int[] resultCom = minimax(gameBoard, player, maxDepth);
+                int row = resultCom[0];
+                int col = resultCom[1];
+                if (gameBoard[row][col] == ' ') {
+                    gameBoard[row][col] = 'O';
+                    if (checkIfWinner(gameBoard, player)) {
+                        gameRunning = false;
+                        System.out.println("Computer Wins");
+                    }
+                    player = !player;
+                }
             } else {
                 myEventSC.nextEvent();
             }
+
+            if (checkIfFull(gameBoard) && gameRunning) { //Check draw
+                gameRunning = false;
+                System.out.println("Unentschieden");
+            }
+            drawGameBoard(myDrawObj, gameBoard);
         }
+        myDrawObj.close();
     }
 
-    private static int[] minimax(char[][] gameBoard, boolean player, int depth) {
-
-        int[] retArray = new int[3]; // [row, col, score]
+    public static int[] minimax(char[][] gameBoard, boolean player, int depth) {
+        int[] retArray = new int[3];
 
         if (player) {
-            retArray[2] = Integer.MIN_VALUE;
-        } else {
             retArray[2] = Integer.MAX_VALUE;
+        } else {
+            retArray[2] = Integer.MIN_VALUE;
         }
 
         for (int row = 0; row < gameBoard.length; row++) {
             for (int col = 0; col < gameBoard[row].length; col++) {
-                if (gameBoard[row][col] == ' ') { // Empty field
-                    gameBoard[row][col] = player ? 'X' : 'O';
+
+                if (gameBoard[row][col] == ' ') {
+                    if (player) {
+                        gameBoard[row][col] = 'X';
+                    } else {
+                        gameBoard[row][col] = 'O';
+                    }
 
                     if (checkIfWinner(gameBoard, true)) {
                         retArray[0] = row;
@@ -59,25 +141,31 @@ public class Aufgabe1 {
                         retArray[0] = row;
                         retArray[1] = col;
                         retArray[2] = 1;
-                    } else if (checkIfFull(gameBoard) || depth - 1 == 0) {
+                    }
+                    else if (checkIfFull(gameBoard) || depth - 1 == 0) {
                         retArray[0] = row;
                         retArray[1] = col;
                         retArray[2] = 0;
-                    } else {
+                    }
+                    else {
                         int[] tempArray = minimax(gameBoard, !player, depth - 1);
 
-                        if (player && tempArray[2] > retArray[2]) {
-                            retArray[0] = row;
-                            retArray[1] = col;
-                            retArray[2] = tempArray[2];
-                        } else if (!player && tempArray[2] < retArray[2]) {
-                            retArray[0] = row;
-                            retArray[1] = col;
-                            retArray[2] = tempArray[2];
+                        if (player) {
+                            if (tempArray[2] < retArray[2]) {
+                                retArray[0] = row;
+                                retArray[1] = col;
+                                retArray[2] = tempArray[2];
+                            }
+                        } else {
+                            if (tempArray[2] > retArray[2]) {
+                                retArray[0] = row;
+                                retArray[1] = col;
+                                retArray[2] = tempArray[2];
+                            }
                         }
                     }
 
-                    gameBoard[row][col] = ' '; // Undo move
+                    gameBoard[row][col] = ' ';
                 }
             }
         }
@@ -89,11 +177,11 @@ public class Aufgabe1 {
         for (char[] array : gameBoard) {
             for (char element : array) {
                 if (element == ' ') {
-                    return true;
+                    return false;
                 }
             }
         }
-        return false; //Zeile kann geändert oder entfernt werden.
+        return true; //Zeile kann geändert oder entfernt werden.
     }
 
     private static boolean checkIfWinner(char[][] gameBoard, boolean player) {
